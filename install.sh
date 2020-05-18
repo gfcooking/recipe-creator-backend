cd75ab002fa3b163d9ac033052eac5dde6af98ca
Matthew D. Scholefield <matthew331199@gmail.com>
2020-05-18 02:27:47 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,16 @@ if [ ! -f 'start.sh' ]; then
     exit 1
 fi
 
+set -eE
+[ -f .venv/bin/pip ] || python3 -m venv .venv/
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
 escaped_path=$(pwd | sed -e 's/[\/&]/\\&/g')
 
 echo "Writing systemd service..."
-cat recipe-creator-backend.service.template | sed -e 's/%%PATH%%/'"$escaped_path"'/gm' | sudo tee /etc/systemd/recipe-creator-backend.service
+cat recipe-creator-backend.service.template | sed -e 's/%%PATH%%/'"$escaped_path"'/gm' | sudo tee /etc/systemd/system/recipe-creator-backend.service
+sudo systemctl daemon-reload
 echo "Enabling service..."
 sudo systemctl enable recipe-creator-backend
 echo "Starting service..."
